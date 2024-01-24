81ea8040c2a5da4bbff1ba85fa1eac942890e1f5
Marius Zeinhofer <marius.zeinhofer@gmail.com>
2024-01-24 14:28:57 +0100

    corrected lint-ruff.yaml

---
+++ sample/sample_computation.py
@@ -0,0 +1,13 @@
+"""A testable function that does some computation."""
+
+
+def quadratics(n: int) -> int:
+    """A function computing quadratics.
+
+    Args:
+        n: The number to square.
+
+    Returns:
+        The square of n.
+    """
+    return n**2
--- sample/sample_file.py
+++ sample/sample_file.py
@@ -1,24 +1,12 @@
 """A sample file that prints sample."""
 
 
-def sample_function(k: int = 2):
-    """Compute the batched Hessian of the model w.r.t. its input.
+def sample_function(greeting: str = "Greetings."):
+    """A function that is there just as a sample.
 
     Args:
-        k: stuff.
-        model: The model whose Hessian will be computed. Must produce batched scalars as
-            output.
-        X: The input to the model. First dimension is the batch dimension.
-            Must be differentiab .
-            le.
+        greeting: The greeting uttered by this function.
 
-    Returns:
-        The Hessians of the model w.r.t. X. Has shape
-        `[batch_size, *X.shape[1:], *X.shape[1:]]`.
+    Returns: Nothing.
     """
     print("Just output from a sample function.")
-
-
-print("Hi there, I am mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm just a sample.")
-
-x = 2 *3 - 4
