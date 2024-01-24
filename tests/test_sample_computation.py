9ad4144d6f6cfb2a92d6645233163c7fe51a0878
Marius Zeinhofer <marius.zeinhofer@gmail.com>
2024-01-24 15:33:37 +0100

    testing that workflows captures a faulty test

--- tests/test_sample_computation.py
+++ tests/test_sample_computation.py
@@ -5,4 +5,4 @@ from sample.sample_computation import quadratics
 
 def test_answer():
     """We test the quadratics function at one input."""
-    assert quadratics(3) == 9
+    assert quadratics(3) == 90
