fa231c833ff707b70496564983fa7cab30d96976
Marius Zeinhofer <marius.zeinhofer@gmail.com>
2024-01-24 14:07:05 +0100

    test

--- README.md
+++ README.md
@@ -13,8 +13,23 @@ conda activate sample_project
 
 ## Editable Install with PIP
 
-Install the package in editable mode
+In case you are not using conda you can install the package 
+in editable mode using:
 
 ```bash
-pip install -e .
+pip install -e ."[lint,test]"
 ```
+
+## Linting and Formatting
+
+We use ruff for linting and formatting:
+
+```bash
+ruff check .
+ruff format .
+```
+
+## Github Workflows
+
+Upon pushing checks are carried out through Github Workflows.
+The checks run the ruff linter and execute the tests.
--- sample/sample_file.py
+++ sample/sample_file.py
@@ -1,7 +1,23 @@
 """A sample file that prints sample."""
 
-def sample_function():
-    print('Just output from a sample function.')
+
+def sample_function(k: int = 2):
+    """Compute the batched Hessian of the model w.r.t. its input.
+
+    Args:
+        model: The model whose Hessian will be computed. Must produce batched scalars as
+            output.
+        X: The input to the model. First dimension is the batch dimension.
+            Must be differentiab .
+            le.
+
+    Returns:
+        The Hessians of the model w.r.t. X. Has shape
+        `[batch_size, *X.shape[1:], *X.shape[1:]]`.
+    """
+    print("Just output from a sample function.")
 
 
-print('Hi there, I am just a sample.')
+print("Hi there, I am mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm just a sample.")
+
+x = 2 *3 - 4
